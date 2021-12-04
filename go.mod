3d80561bb9fbfdf62ca56b3dc94e9f0442ad01c4
Baptiste Rousseau <baptiste.rousseau@gmail.com>
2021-12-04 13:35:22 +0100

    added dockerfile

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module fizzbuzz
 
 go 1.17
+
+require (
+	github.com/go-chi/chi v1.5.4
+	github.com/go-chi/cors v1.2.0
+	github.com/go-chi/render v1.0.1
+)
--- main.go
+++ main.go
@@ -1,15 +1,51 @@
 package main
 
 import (
-	"fmt"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/cors"
+	"github.com/go-chi/render"
+	"log"
+	"net/http"
 	"strconv"
 )
 
-func main() {
-	fizzbuzz(4, 5, 10000, "hello", "world")
+func Routes() *chi.Mux {
+	router := chi.NewRouter()
+
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+	})
+
+	router.Use(
+		c.Handler,
+		render.SetContentType(render.ContentTypeJSON),
+		middleware.Logger,
+		middleware.RedirectSlashes,
+		middleware.Recoverer,
+	)
+
+	//public routes
+	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		render.Status(r, http.StatusOK)
+		render.JSON(w, r,  fizzbuzz(4, 5, 10000, "hello", "world"))
+	})
+
+	return router
 }
 
-func fizzbuzz(int1 int, int2 int, limit int, str1 string, str2 string) {
+func main() {
+	router := Routes()
+
+	if err := http.ListenAndServe("localhost:8080", router); err != nil {
+		log.Fatalln("Server error:", err.Error())
+	}
+}
+
+func fizzbuzz(int1 int, int2 int, limit int, str1 string, str2 string) string {
 	var output string
 
 	for i:=1; i<=limit; i++ {
@@ -29,5 +65,5 @@ func fizzbuzz(int1 int, int2 int, limit int, str1 string, str2 string) {
 		}
 	}
 
-	fmt.Println(output)
+	return output
 }
